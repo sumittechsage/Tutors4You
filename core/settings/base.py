fb749dda1b1787cb42e9ac0408903299851bbcc7
Saggarwal <saggarwal@innow8apps.com>
2024-09-22 13:17:13 +0530

    BASE DIR

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+print("BASE_DIR: ", BASE_DIR )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
