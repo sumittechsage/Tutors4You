fd66aa2ad9cf99b4e9ef67753df191d57021dd6b
Saggarwal <saggarwal@innow8apps.com>
2024-09-22 13:07:14 +0530

    settings for render

--- core/settings/render.py
+++ core/settings/render.py
@@ -10,3 +10,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['https://tutors4you.onrender.com']
+CORS_ALLOWED_ORIGINS = ['https://tutors4you.onrender.com']
